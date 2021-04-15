8faf5ee1d2586c8ee8976030a967878f8c4f73ad
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2021-04-15 13:57:47 +0300

    Project file added

---
+++ Assets/Scripts/BoxFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoxFollow : MonoBehaviour
+{
+
+    public NavMeshAgent box;
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        box.SetDestination(player.position);
+    }
+}
